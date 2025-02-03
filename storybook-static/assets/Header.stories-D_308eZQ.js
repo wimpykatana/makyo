ba8aa4bfc08dbb42039f9b71bcc631609c0b352c
Wimpy Katana <wim.katana@gmail.com>
2025-02-03 16:00:02 +0700

    remove example

---
+++ storybook-static/assets/Header.stories-D_308eZQ.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-Bn9rm4iU.js";import{H as p}from"./Header-xWraX0Q6.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./Button-VBjU_o9A.js";const f={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,f as default};
